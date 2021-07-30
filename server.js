796d7af2da5dcd0fe2a2d031fbf1865caf28e0dd
baxy5 <bakos.soda@gmail.com>
2021-07-30 13:04:35 +0200

    basic express routing

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || "5000";
+
+app.use(express.static("public"));
+
+app.get("/index.html", (req, res, next) => {
+  res.send("index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT} port!`);
+});
