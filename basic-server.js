0d38b033c0df4612b6b6bb8fe5953f927d10ecce
baxy5 <bakos.soda@gmail.com>
2021-07-30 15:55:58 +0200

    basic routing

--- basic-server.js
+++ basic-server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || "5000";
 app.use(express.static("public"));
 
 app.get("/index.html", (req, res, next) => {
-  res.send("index.html");
+  res.send("public/index.html");
 });
 
 app.listen(PORT, () => {
