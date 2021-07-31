51a08d76436f766e0023b708a9b85f116fdbecc1
baxy5 <bakos.soda@gmail.com>
2021-07-31 12:00:59 +0200

    status error and objects

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+const index = {
+    audi: {
+        evjarat: "2001",
+        tipus: "tt"
+    },
+    bmw: {
+        evjarat: "2005",
+        tipus: "120d"
+    }
+}
+
+app.use(express.static("public"));
+
+app.get("/index", (req, res) => {
+    const isError = index.audi.evjarat;
+
+    if(isError){
+        res.send(isError);
+    }else{
+        res.status(404).send("Error");
+    }
+});
 
 
-app.listen(PORT, ()=> {
-    console.log(`The server is running on ${PORT} port!`);
-})
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT} port!`);
+});
--- basic-server.js
+++ basic-server.js
@@ -3,10 +3,11 @@ const app = express();
 
 const PORT = process.env.PORT || "5000";
 
+
 app.use(express.static("public"));
 
-app.get("/index.html", (req, res, next) => {
-  res.send("public/index.html");
+app.get("/index", (req, res, next) => {
+  console.log(req);
 });
 
 app.listen(PORT, () => {
